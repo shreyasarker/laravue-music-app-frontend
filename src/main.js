dd94ff008d00ff674cbf1e411badc6d60f14c9fd
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-18 12:57:09 +0600

    Removed Unecessary Views and Components files

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import './registerServiceWorker';
 import router from './router';
 import './assets/tailwind.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
