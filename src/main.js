80b7fea1829d574b6b6325fd5ebd8350d0d1725b
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-18 12:51:12 +0600

    Installed Necessary Packages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import './assets/tailwind.css';
 
 createApp(App).use(router).mount('#app')
