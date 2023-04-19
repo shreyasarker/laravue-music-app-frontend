97e24dff0659fee01c1ccc255472cf84c601ca2f
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-19 16:10:47 +0600

    Feat Profile UI: Some button rearranged

--- src/components/partials/profile/ProfileSongSection.vue
+++ src/components/partials/profile/ProfileSongSection.vue
@@ -2,13 +2,9 @@
   <div class="w-full py-4">
     <div class="text-white text-xl mb-2">Songs</div>
     <div class="bg-purple-700 w-full h-1"></div>
-    <div class="flex justify-evenly my-4">
-      <span class="flex">
-        <CustomButton btn-text="Add Song" :url="{}" class="text-white text-sm"/>
-      </span>
-      <span class="flex">
-        <CustomButton btn-text="Delete Song" :url="{}" class="text-white text-sm" />
-      </span>
+    <div class="flex justify-end my-4">
+      <CustomButton btn-text="Add Song" :url="{}" class="text-white text-sm mr-2"/>
+      <CustomButton btn-text="Delete Song" :url="{}" class="text-white text-sm" />
     </div>
     <div class="pb-4">
       <ProfileSongPlayer />
