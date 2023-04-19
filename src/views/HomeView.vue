422dc43d2167f19893a637ed426e58f10408735c
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-19 19:27:40 +0600

    Feat: Top Navigation UI added to Homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
+  <TopNavigation />
   <div class="home">
     <HeroSection />
   </div>
 </template>
 
 <script setup>
+import TopNavigation from '@/components/layouts/TopNavigation.vue';
 import HeroSection from '../components/partials/HeroSection.vue';
 
 </script>
