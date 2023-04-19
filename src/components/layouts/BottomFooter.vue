5b0bc550b2d5ba2a2cd15288c5dca70d739f68e6
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-19 20:07:32 +0600

    Feat: Footer added in Profile Section

---
+++ src/components/layouts/BottomFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full bg-black border-t border-gray-300">
+    <footer class="">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <router-link to="/" class="flex items-center mb-4 sm:mb-0">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">MusicApp</span>
+          </router-link>
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">Terms & Conditions</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <hr class="my-6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center text-gray-400">&copy; 2023 MusicApp. All Rights Reserved.</span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
--- src/views/account/AccountView.vue
+++ src/views/account/AccountView.vue
@@ -1,14 +1,16 @@
 <template>
   <TopNavigation />
-  <div class="account-section bg-gray-950">
+  <div class="account-section bg-gray-950 pb-6">
     <div class="max-w-4xl mx-auto">
       <router-view />
     </div>
   </div>
+  <BottomFooter />
 </template>
 
 <script setup>
 import TopNavigation from '@/components/layouts/TopNavigation.vue';
+import BottomFooter from '@/components/layouts/BottomFooter.vue';
 
 </script>
 
