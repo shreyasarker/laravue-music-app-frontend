0ac6c7024263cc2e9f86d3256da0e06768f3e27f
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-20 19:40:02 +0600

    Feat: Added Posts Page in Menu

--- src/components/layouts/TopNavigation.vue
+++ src/components/layouts/TopNavigation.vue
@@ -21,6 +21,11 @@
                 Profile
               </router-link>
             </li>
+            <li>
+              <router-link :to="{name: 'account.profile.posts'}" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 hover:text-purple-700 md:hover:bg-transparent md:border-0 md:hover:text-purple-400 md:p-0">
+                Posts
+              </router-link>
+            </li>
           </ul>
         </div>
       </div>
