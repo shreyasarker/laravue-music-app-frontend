258c03e376c224cd12096bb559f48b1b6fc1b059
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-23 03:08:40 +0600

    Feat User Profile: Shown Name, Location, Description, Image

--- src/components/partials/profile/ProfileAboutSection.vue
+++ src/components/partials/profile/ProfileAboutSection.vue
@@ -2,18 +2,25 @@
   <main class="relative z-20 w-full mt-8 md:flex md:items-center xl:mt-12">
     <div class="absolute w-full bg-purple-600 -z-10 md:h-96 rounded-2xl"></div>
     <div class="w-full p-6 bg-blue-600 md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
-      <img class="h-24 w-24 md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[26rem] md:rounded-2xl" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="client photo" />   
+      <img :src="authUser.image" class="h-24 w-24 md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[26rem] md:rounded-2xl" alt="Profile photo" />   
       <div class="mt-2 md:mx-6">
         <div>
-          <p class="text-xl font-medium tracking-tight text-white">Ema Watson</p>
-          <p class="text-blue-200 ">Marketing Manager at Stech</p>
+          <p class="text-xl font-medium tracking-tight text-white">{{ authUser.name }}</p>
+          <p class="text-blue-200 ">{{ authUser.location }}</p>
         </div>
-        <p class="mt-4 text-lg leading-relaxed text-white md:text-xl"> “Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda”.</p>
+        <p class="mt-4 text-lg leading-relaxed text-white md:text-xl">{{ authUser.description }}</p>
       </div>
     </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+import { useAuthStore } from '@/store/auth.store.js';
+const authStore = useAuthStore();
+
+const authUser = computed(() => authStore.auth);
+
+</script>
 
 <style></style>
