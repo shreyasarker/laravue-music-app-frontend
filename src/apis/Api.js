a3b4f128a0c2955bc7b39aa63e3f0f09d819354c
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-23 02:31:03 +0600

    Added ENV

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.VUE_APP_API_URL,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
