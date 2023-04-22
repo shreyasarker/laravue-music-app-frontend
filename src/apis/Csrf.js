9124a5d8111a6c3fb1aa8286b2e0efba5bd0d1ad
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-23 02:35:18 +0600

    Install JS Cookie to check existing CSRF Token

--- src/apis/Csrf.js
+++ src/apis/Csrf.js
@@ -1,6 +1,14 @@
+import Cookie from 'js-cookie';
 import Api from './Api.js';
 
 const Csrf = () => {
+  let token = Cookie.get('XSRF-TOKEN');
+
+  if(token) {
+    return new Promise((resolve) => {
+      resolve(token);
+    })
+  }
   return Api.get('/csrf-cookie');
 }
 
