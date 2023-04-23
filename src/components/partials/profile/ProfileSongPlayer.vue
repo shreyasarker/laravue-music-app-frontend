0658fb108c7e31dc6eeddcdf20feb9f21c4ce2be
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-23 20:22:48 +0600

    Feat Song: Implemented Show Songs in Profile

--- src/components/partials/profile/ProfileSongPlayer.vue
+++ src/components/partials/profile/ProfileSongPlayer.vue
@@ -5,51 +5,26 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { computed, watch } from 'vue';
 import APlayer from 'aplayer';
 import 'aplayer/dist/APlayer.min.css';
+import { useSongStore } from '@/store/song.store.js';
+
+const songStore = useSongStore();
+const songs = computed(() => songStore.songs);
 
 const callThePlayer = () => {
   new APlayer({
     container: document.getElementById('aplayer'),
     theme: '#000000',
-    audio: [
-    {
-      name: 'Cyber Punk',
-      artist: 'artist',
-      url: '/music/cyber-punk-21548.mp3',
-      cover: 'cover.jpg'
-    },
-    {
-      name: 'Hip Hop Bach',
-      artist: 'artist',
-      url: '/music/hip-hop-bach-127187.mp3',
-      cover: 'cover.jpg'
-    },
-    {
-      name: 'Ethnic Background',
-      artist: 'artist',
-      url: '/music/ethnic-background-music-for-short-video-1-minute-dance-hip-hop-beat-144565.mp3',
-      cover: 'cover.jpg'
-    },
-    {
-      name: 'Let it go',
-      artist: 'artist',
-      url: '/music/let-it-go-12279.mp3',
-      cover: 'cover.jpg'
-    },
-    {
-      name: 'Dead by Daylight',
-      artist: 'artist',
-      url: '/music/dead-by-daylight-10243.mp3',
-      cover: 'cover.jpg'
-    }
-  ]
-});
+    audio: songs.value
+  });
 }
 
-onMounted(() => {
-  callThePlayer();
+watch(() => songs.value, (newModel) => {
+  if(newModel) {
+    callThePlayer();
+  }
 });
 
 </script>
--- src/store/song.store.js
+++ src/store/song.store.js
@@ -3,7 +3,22 @@ import Api from '@/apis/Api';
 import Csrf from '@/apis/Csrf';
 
 export const useSongStore = defineStore('song', {
+  state: () => ({
+    songs: []
+  }),
   actions: {
+    getSongs() {
+      return new Promise((resolve, reject) => {
+        Csrf();
+        Api.get('/songs')
+        .then((response) => {
+          this.songs = response.data.data;
+          resolve(response);
+        }).catch((error) => {
+          reject(error);
+        })
+      });
+    },
     storeSong(data) {
       return new Promise((resolve, reject) => {
         Csrf();
