030eb1054280e847bf09395e2a03cc26bc983c0d
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-26 01:50:05 +0600

    Feat Nav: Logic added for Logout Meny

--- src/components/layouts/TopNavigation.vue
+++ src/components/layouts/TopNavigation.vue
@@ -26,7 +26,7 @@
                 Posts
               </router-link>
             </li>
-            <li>
+            <li v-if="authUser.id">
               <a @click="logout" href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 hover:text-purple-700 md:hover:bg-transparent md:border-0 md:hover:text-purple-400 md:p-0">
                 Logout
               </a>
