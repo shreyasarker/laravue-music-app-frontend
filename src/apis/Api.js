e22f2fc3ecbb3f7d6cce5466a81249ae2e9d3854
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-28 17:11:30 +0600

    Converted VueCli to Vite

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: import.meta.env.VITE_APP_API_URL,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
